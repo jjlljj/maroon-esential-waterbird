ef0013a8a1244adf43210426337a81d5aeb6be43
jjlljj <logueja@gmail.com>
2020-04-13 14:05:08 -0600

    Update App.js, App directory structure

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          React Boilerplate | James Logue | <a style={{color: '#f2f2f2'}} href="https://github.com/jjlljj/react-boilerplate" target="_blank">github.com/jjlljj/react-boilerplate</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
